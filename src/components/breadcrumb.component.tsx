b2cb8efe767bab0c3b4f61528b7d1c7509ec957b
Ramesh Ramalingam <rrama@thoughtworks.com>
2020-01-21 12:41:24 +0530

    Made changes to make breadcrumbs customisable (#14)
    * Added logic to get breadcrumbs from configs
    
    * Added margin for breadcrumbs
    
    * 0.0.13

--- src/components/breadcrumb.component.tsx
+++ src/components/breadcrumb.component.tsx
@@ -1,9 +1,10 @@
 import React, { Fragment } from "react";
 
 export default function Breadcrumb(props: BreadcrumbProps) {
+  const marginHorizonal3Style = { margin: "0px 3px" };
   const getPathLink = ({ name, href }: BreadcrumbPath) => (
     <Fragment key={name}>
-      <i className="icon-chevron-right link"></i>
+      <i className="icon-chevron-right link" style={marginHorizonal3Style}></i>
       {href ? <a href={href}>{name}</a> : <span>{name}</span>}
     </Fragment>
   );
